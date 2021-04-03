45f9cde7935a0a7bda670c2af8f8f3baf8849b4b
Nikin Nagewadia <nikin.n@gmail.com>
2021-04-03 23:44:48 +0100

    Add links in bio

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="section" id="about">
     <div class="container">
-      <h2 class="title is-capitalized is-2">About</h2>
+      <h2 class="title is-capitalized is-3">About</h2>
       <div class="columns">
         <div class="column is-two-thirds">
-          <p>Although he graduated with a degree in film studies, Nikin’s interest in technology and design led him to a career in UX design. From Freshbooks to Say Yeah, Nikin helped craft interfaces and front-end code that balanced business and user objectives.</p>
+          <p>Although he graduated with a degree in film studies, Nikin’s interest in technology and design led him to a career in UX design. From <a href="https://www.freshbooks.com/" rel="noreferrer">Freshbooks</a> to <a href="https://sayyeah.com/" rel="noreferrer">Say Yeah</a>, Nikin helped craft interfaces and front-end code that balanced business and user objectives.</p>
 
-          <p>Having spent all his life in Canada, Nikin recently moved to London, England where he’s now focused on supporting the Government Digital Service as an interaction designer.</p>
+          <p>Having spent all his life in Canada, Nikin recently moved to London, England where he’s now focused on supporting the <a href="https://www.gov.uk/government/organisations/government-digital-service" rel="noreferrer">Government Digital Service</a> as an interaction designer.</p>
 
           <p>Aside from his day-to-day work, Nikin is an avid photographer, aiming to capture the everyday as cinematic.</p>
         </div>
